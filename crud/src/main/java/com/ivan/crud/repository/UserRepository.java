a9387998c430d65e9698603e904cda405d01f278
GOD <masterionic@gmail.com>
2020-04-20 22:58:57 +0800

    final completion

---
+++ crud/src/main/java/com/ivan/crud/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.ivan.crud.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ivan.crud.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
+
